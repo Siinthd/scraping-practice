426f0838c7b769b41cc6fc598f3e522479eb9e22
Siinthd <42109629+Siinthd@users.noreply.github.com>
2022-12-05 16:30:19 +0300

    some_fixes
    just save file

--- Goodgame_test.py
+++ Goodgame_test.py
@@ -23,20 +23,25 @@ if not os.path.exists('data_capture'):
     os.mkdir('data_capture')
 
 while True:
-    os.system('cls')
-    response = requests.get(f'https://hls.goodgame.ru/hls/{streamer}.m3u8', headers=headers)
-    queue = []
-    for i in response.text.replace(',','').strip().split('\n'):
-        if i[0] != '#'and i not in queue:
-            queue.append(i)
-    EXTTime = response.text.strip().split('\n')[-2].replace(',','')
-    time.sleep(float(EXTTime.split(':')[1]))
-    for ExtFile in queue:
-        if ExtFile not in last_files:
-            filets = requests.get('https://hls.goodgame.ru/hls/' + ExtFile, headers=headers)
-            with open(f'data_capture/{streamer}.ts','ab') as file:
-                file.write(filets.content)
-                last_files.append(ExtFile)
+    try:
+        response = requests.get(f'https://hls.goodgame.ru/hls/{streamer}.m3u8', headers=headers)
+        queue = []
+        for i in response.text.replace(',','').strip().split('\n'):
+            if i[0] != '#'and i not in queue:
+                queue.append(i)
+        EXTTime = response.text.strip().split('\n')[-2].replace(',','')
+        time.sleep(float(EXTTime.split(':')[1]))
+        for ExtFile in queue:
+            if ExtFile not in last_files:
+                filets = requests.get('https://hls.goodgame.ru/hls/' + ExtFile, headers=headers)
+                with open(f'data_capture/{streamer}.ts','ab') as file:
+                    file.write(filets.content)
+                    last_files.append(ExtFile)
+    except Exception as ex:
+        print(ex)
+        last_files = []
+        queue = []
+        continue
 
 
 
