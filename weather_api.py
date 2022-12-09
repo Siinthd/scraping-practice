71876fd2e037c455b18834e80e879e08f6b8e0be
Siinthd <42109629+Siinthd@users.noreply.github.com>
2022-12-09 09:22:53 +0300

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -2,11 +2,12 @@ import os
 import requests
 import time
 import json
-from datetime import datetime
+from datetime import datetime,timedelta
 from aiogram import Bot,Dispatcher,executor,types
 from aiogram.dispatcher.filters import Text
+from aiogram.utils.markdown import hlink,hbold
 
-tbot = Bot(token=os.getenv('tgTOKEN'))
+tbot = Bot(token=os.getenv('tgTOKEN'),parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(tbot)
 
 API_key = os.getenv('openweathermap_token')
@@ -26,12 +27,16 @@ async def start(message: types.Message):
 @dp.message_handler(Text(equals='Узнать погоду'))
 async def moscow_city(message: types.Message):
     await message.answer("Ждите")
-    file = get_weather("Pskov")[0]
-    await message.reply(f"***{file.get('Время запроса')}***\n"
-                        f"{file.get('Погода')}\n"
-                        f"{file.get('Значок погоды')}\n"
-                        f"{file.get('Минимальная температура') - 273.13} - "
-                        f"{file.get('Максимальная температура')- 273.13}\n")
+    last_request = datetime.now()
+    if os.path.exists('data_capture/weather.json'):
+        with open('data_capture/weather.json',encoding='UTF-8') as file:
+            weather = json.load(file)
+        if (last_request-datetime.strptime(weather[0].get('Время запроса'),'%Y-%m-%d %H:%M:%S')) > timedelta(minutes = 59):
+            weather = get_weather("Pskov")
+    else:
+        weather = get_weather("Pskov")
+    await message.answer(f"{hlink(weather[0].get('Погода'),weather[0].get('Значок погоды'))}\n{weather[0].get('Минимальная температура')}\u00b0C....{weather[0].get('Максимальная температура')}\u00b0C\n")
+
 
 def get_weather(city_name):
     result = []
@@ -39,7 +44,7 @@ def get_weather(city_name):
         url_city = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit={limit}&appid={API_key}'
         city_coords = requests.get(url_city)
         for i in city_coords.json():
-            weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={i.get("lat")}&lon={i.get("lon")}&appid={API_key}&lang={"ru"}').json()
+            weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={i.get("lat")}&lon={i.get("lon")}&appid={API_key}&lang={"ru"}&units=metric').json()
             try:
                 result.append({
                 "Рассвет" : datetime.fromtimestamp(weather.get('sys').get('sunrise')).strftime('%Y-%m-%d %H:%M:%S'),
@@ -62,8 +67,9 @@ def get_weather(city_name):
 
     except Exception as ex:
         print(ex)
+    with open('data_capture/weather.json','w',encoding = 'UTF-8') as file:
+        json.dump(result,file,indent=4,ensure_ascii = False)
     return result
 
 if __name__=='__main__':
-    # executor.start_polling(dp)
-    print(get_weather("Pskov")[0])
+    executor.start_polling(dp)
